b1d737c9486408520d564e0b4e204c388c23ee3c
Confocus <359904792@qq.com>
2022-01-25 17:32:42 +0800

    inject blank function success

--- DllInject/main.cpp
+++ DllInject/main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <Windows.h>
-
+#include <string>
 //#define MAX_DLLNAME_LEN	0x20
 //#define HOOK_API_DLL	"HookApi.dll"
 //#define HOOK_API_DLL	"C:/Code/R3UnhookDemo/R3UnhookDemo/x64/Debug/HookApi64.dll"
-#define HOOK_API_DLL	"D:\\Coding\\HookSeries\\HookSeries\\x64\\Debug\\HookApi.dll"
+#define HOOK_API_DLL	"C:\\Code\\R3UnhookDemo\\R3UnhookDemo\\x64\\Debug\\HookApi.dll"
+#define INLINE_HOOK_THREAD_SIZE	1024
 
 typedef DWORD(*LPZwCreateThreadEx)(
 
@@ -36,26 +37,21 @@ typedef HMODULE(*LPLoadLibraryA)(
 
 BOOL EnableDebugPriv(LPCSTR name);
 
-#ifdef WIN32
 BOOL WINAPI InjectTargetProcess(DWORD dwProcessId);
-#else
-BOOL __fastcall InjectTargetProcess(DWORD dwProcessId);
-#endif
+BOOL WINAPI InjectTargetProcess2(DWORD dwProcessId);
 
 int main()
 {
+	OutputDebugStringA("[HookSeris]main start.\n");
+
 	BOOL bSuccess = FALSE;
 	EnableDebugPriv("SeDebugPrivilege");
-	printf("path:%s\n", HOOK_API_DLL);
-	bSuccess = InjectTargetProcess(2616);
+	//bSuccess = InjectTargetProcess(2616);
+	bSuccess = InjectTargetProcess2(15028);
 	return 1;
 }
 
-#ifdef WIN32
 BOOL WINAPI InjectTargetProcess(DWORD dwProcessId)
-#else
-BOOL __fastcall InjectTargetProcess(DWORD dwProcessId)
-#endif
 {
 	BOOL bRet = FALSE;
 	HANDLE hProcess = NULL;
@@ -111,9 +107,15 @@ BOOL __fastcall InjectTargetProcess(DWORD dwProcessId)
 			break;
 		}
 
+		//ZwCreateThreadEx可以注入Win10下的计算器
 		dwStatus = pZwCreateThreadEx(&hRemoteThread, PROCESS_ALL_ACCESS, NULL, hProcess, (LPTHREAD_START_ROUTINE)pLoadLibraryA, pTargetMem, 0, 0, 0, 0, NULL);
-		dwLastError = GetLastError();
+		if (0 != dwStatus)
+		{
+			dwLastError = GetLastError();
+			break;
+		}
 
+		//CreateRemoteThread无法注入Win10下的计算器
 		/*hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)pLoadLibraryA, pTargetMem, 0, NULL);
 		dwLastError = GetLastError();
 		if (NULL == hThread)
@@ -121,7 +123,7 @@ BOOL __fastcall InjectTargetProcess(DWORD dwProcessId)
 			break;
 		}*/
 
-		WaitForSingleObject(hThread, INFINITE);
+		WaitForSingleObject(hRemoteThread, INFINITE);
 		bRet = TRUE;
 	} while (FALSE);
 
@@ -133,6 +135,106 @@ BOOL __fastcall InjectTargetProcess(DWORD dwProcessId)
 	if (hThread)
 	{
 		CloseHandle(hThread);
+	}
+
+	if (hRemoteThread)
+	{
+		CloseHandle(hRemoteThread);
+	}
+
+	return bRet;
+}
+
+//这里如果不设置为static，则printf("InlineHookThread:0x%016I64x\n", ::InlineHookThread);打印的地址可能是错误的
+static DWORD WINAPI InlineHookThread(LPVOID pParam)
+{
+	//OutputDebugStringA("[HookSeris]CreateThread InlineHookThread.\n");
+	/*std::string s = "ttt";
+	MessageBoxA(0, s.c_str(), s.c_str(), 0);*/
+	return 1;
+}
+
+BOOL WINAPI InjectTargetProcess2(DWORD dwProcessId)
+{
+	BOOL bRet = FALSE;
+	HANDLE hProcess = NULL;
+	HANDLE hThread = NULL;
+	HANDLE hRemoteThread = NULL;
+	LPVOID pThreadMem = NULL;
+	SIZE_T dwWritten = 0;
+	HMODULE hModNtdll = NULL;
+	HMODULE hMod = NULL;
+	DWORD dwLastError = 0;
+	DWORD dwStatus = 0;
+	LPZwCreateThreadEx pZwCreateThreadEx = NULL;
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId);
+	if (NULL == hProcess)
+	{
+		return FALSE;
+	}
+
+	do
+	{
+		pThreadMem = VirtualAllocEx(hProcess, NULL, INLINE_HOOK_THREAD_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+		if (NULL == pThreadMem)
+		{
+			break;
+		}
+
+		printf("InlineHookThread:0x%016I64x\n", ::InlineHookThread);
+		printf("InlineHookThread:0x%016I64x\n", *InlineHookThread);
+		printf("InlineHookThread:0x%llx\n", &InlineHookThread);//一样
+		//这里直接打印的是jmp     InlineHookThread 指令的地址。jmp     InlineHookThread为E9 96 0C
+
+		if (!WriteProcessMemory(hProcess, pThreadMem, InlineHookThread, INLINE_HOOK_THREAD_SIZE, &dwWritten))
+		{
+			dwLastError = GetLastError();
+			break;
+		}
+
+		hModNtdll = LoadLibraryA("ntdll.dll");
+		if (NULL == hModNtdll)
+		{
+			break;
+		}
+
+		pZwCreateThreadEx = (LPZwCreateThreadEx)GetProcAddress(hModNtdll, "ZwCreateThreadEx");
+		if (NULL == pZwCreateThreadEx)
+		{
+			break;
+		}
+
+		/*dwStatus = pZwCreateThreadEx(&hRemoteThread, PROCESS_ALL_ACCESS, NULL, hProcess, (LPTHREAD_START_ROUTINE)pThreadMem, NULL, 0, 0, 0, 0, NULL);
+		if (0 != dwStatus)
+		{
+			dwLastError = GetLastError();
+			break;
+		}*/
+
+		hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)pThreadMem, NULL, 0, NULL);
+		dwLastError = GetLastError();
+		if (NULL == hThread)
+		{
+			break;
+		}
+
+		WaitForSingleObject(hRemoteThread, INFINITE);
+		bRet = TRUE;
+	} while (FALSE);
+
+	if (hProcess)
+	{
+		CloseHandle(hProcess);
+	}
+
+	if (hThread)
+	{
+		CloseHandle(hThread);
+	}
+
+	if (hRemoteThread)
+	{
+		CloseHandle(hRemoteThread);
 	}
 
 	return bRet;
--- HookApi/dllmain.cpp
+++ HookApi/dllmain.cpp
@@ -1,19 +1,35 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
 
+void InlineHookThread()
+{
+	OutputDebugStringA("[HookSeris]CreateThread InlineHookThread.\n");
+
+}
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
                      )
 {
+	OutputDebugStringA("[HookSeris]Enter DllMain..\n");
+
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        OutputDebugStringA("attached\n");
+        OutputDebugStringA("[HookSeris]Process attached\n");
+        HANDLE hInlineHookThread = NULL;
+        DWORD dwThreadId = 0;
+        hInlineHookThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)InlineHookThread, NULL, 0, &dwThreadId);
         break;
     case DLL_THREAD_ATTACH:
+		OutputDebugStringA("[HookSeris]Thread attached\n");
+        break;
     case DLL_THREAD_DETACH:
+		OutputDebugStringA("[HookSeris]Thread detached\n");
+        break;
     case DLL_PROCESS_DETACH:
+		OutputDebugStringA("[HookSeris]Process detached\n");
         break;
     }
     return TRUE;
