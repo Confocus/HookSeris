9e9c14c6b58666d74b393c940aab1e4f19b3b73b
Confocus <359904792@qq.com>
2022-01-27 16:58:53 +0800

    start analysis pe, and openprocess still failed.

--- R3UnhookDemo/CR3APIHookScanner.cpp
+++ R3UnhookDemo/CR3APIHookScanner.cpp
@@ -2,7 +2,7 @@
 #include <TlHelp32.h>
 
 vector<PROCESS_INFO*> CR3APIHookScanner::m_vecProcessInfo;//无法解析的外部符号
-vector<MODULE_INFO*> CR3APIHookScanner::m_vecModuleInfo;
+//vector<MODULE_INFO*> CR3APIHookScanner::m_vecModuleInfo;
 
 CR3APIHookScanner::CR3APIHookScanner()
 {
@@ -16,13 +16,29 @@ CR3APIHookScanner::~CR3APIHookScanner()
 
 BOOL CR3APIHookScanner::Init()
 {
+	EnableDebugPrivelege();
 	return TRUE;
 }
 
 BOOL CR3APIHookScanner::ScanAllProcesses()
 {
-	//EmurateProcesses(CbCollectProcessInfo);
-	EmurateModules(27116, CbCollectModuleInfo);
+	//清空上一次扫描的内容
+	Clear();
+	//获取到所有进程
+	if (!EmurateProcesses(CbCollectProcessInfo))
+	{
+		return FALSE;
+	}
+
+	//获取到所有进程的所有模块
+	for (PPROCESS_INFO pProcessInfo : m_vecProcessInfo)
+	{
+		EmurateModules(pProcessInfo, CbCollectModuleInfo);
+		//todo：考虑进程消失的情况和进程ID变动的情况
+		//ScanSingleProcessById(pProcessInfo->dwProcessId);
+		ScanSingle(pProcessInfo);
+	}
+
 	return TRUE;
 }
 
@@ -44,24 +60,38 @@ BOOL CR3APIHookScanner::Release()
 		{
 			if (pProcessInfo)
 			{
+				for (auto pMoudleInfo : pProcessInfo->m_vecModuleInfo)
+				{
+					if (pMoudleInfo)
+					{
+						delete pMoudleInfo;
+						pMoudleInfo = NULL;
+					}
+				}
+
 				delete pProcessInfo;
 				pProcessInfo = NULL;
 			}
 		}
 	}
 
-	if (m_vecModuleInfo.size() > 0)
-	{
-		for (auto pModuleInfo : m_vecModuleInfo)
-		{
-			if (pModuleInfo)
-			{
-				delete pModuleInfo;
-				pModuleInfo = NULL;
-			}
-		}
-	}
+	//if (m_vecModuleInfo.size() > 0)
+	//{
+	//	for (auto pModuleInfo : m_vecModuleInfo)
+	//	{
+	//		if (pModuleInfo)
+	//		{
+	//			delete pModuleInfo;
+	//			pModuleInfo = NULL;
+	//		}
+	//	}
+	//}
 
+	return TRUE;
+}
+
+BOOL CR3APIHookScanner::Clear()
+{
 	return TRUE;
 }
 
@@ -109,10 +139,16 @@ BOOL CR3APIHookScanner::EmurateProcesses(CALLBACK_EMUNPROCESS pCallbackFunc)
 	return TRUE;
 }
 
-BOOL CR3APIHookScanner::EmurateModules(DWORD dwProcessId, CALLBACK_EMUNMODULE pCallbackFunc)
+BOOL CR3APIHookScanner::EmurateModules(PPROCESS_INFO pProcessInfo, CALLBACK_EMUNMODULE pCallbackFunc)
 {
+	//todo：貌似有区别	TH32CS_SNAPMODULE32 | TH32CS_SNAPMODULE ??
+	if (NULL == pProcessInfo)
+	{
+		return FALSE;
+	}
+
 	BOOL bNext = FALSE;
-	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwProcessId);
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pProcessInfo->dwProcessId);
 	if (INVALID_HANDLE_VALUE == hSnapshot)
 	{
 		return FALSE;
@@ -127,18 +163,99 @@ BOOL CR3APIHookScanner::EmurateModules(DWORD dwProcessId, CALLBACK_EMUNMODULE pC
 		pModuleInfo = new(std::nothrow) MODULE_INFO();
 		if (pModuleInfo)
 		{
+			//保存ModuleInfo中必要的数据
+			ZeroMemory(pModuleInfo, sizeof(PMODULE_INFO));
+			pModuleInfo->pDllBaseAddr = ModuleEntry32.modBaseAddr;
+			pModuleInfo->dwSizeOfImage = ModuleEntry32.modBaseSize;
 			wmemcpy_s(pModuleInfo->szModuleName, MAX_MODULE_LEN, ModuleEntry32.szModule, wcslen(ModuleEntry32.szModule));
+			wmemcpy_s(pModuleInfo->szModulePath, MAX_MODULE_PATH, ModuleEntry32.szExePath, wcslen(ModuleEntry32.szExePath));
 		}
 
 		if (pCallbackFunc && pModuleInfo)
 		{
-			pCallbackFunc(pModuleInfo);
+			pCallbackFunc(pProcessInfo, pModuleInfo);
 		}
 
 		bNext = Module32Next(hSnapshot, &ModuleEntry32);
 	}
 
 	CloseHandle(hSnapshot);
+	return TRUE;
+}
+
+BOOL CR3APIHookScanner::ScanSingle(PPROCESS_INFO pProcessInfo)
+{
+	if (NULL == pProcessInfo)
+	{
+		return FALSE;
+	}
+
+	//todo：验证这个pid对应的是之前的那个程序
+	BOOL bIsWow64 = FALSE;
+	DWORD dwErrCode = 0;
+	HANDLE hProcess = NULL;
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pProcessInfo->dwProcessId);
+	if (NULL == hProcess)
+	{
+		dwErrCode = GetLastError();
+		return FALSE;
+	}
+
+	IsWow64Process(hProcess, &bIsWow64);
+	for (auto pModuleInfo : pProcessInfo->m_vecModuleInfo)
+	{
+		LoadDllImage(pModuleInfo->szModulePath);
+	}
+
+	CloseHandle(hProcess);
+
+	return TRUE;
+}
+
+BOOL CR3APIHookScanner::LoadDllImage(PWCHAR pDllPath)
+{
+	if (NULL == pDllPath)
+	{
+		return FALSE;
+	}
+
+	printf("Dll path:%ls\n", pDllPath);
+	/*HANDLE hFile = NULL;
+	hFile = CreateFile(pDllPath, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);*/
+
+	return TRUE;
+}
+
+BOOL CR3APIHookScanner::FixRelocData()
+{
+	return TRUE;
+}
+
+BOOL CR3APIHookScanner::EnableDebugPrivelege()
+{
+	HANDLE hToken;
+	LUID sedebugnameValue;
+	TOKEN_PRIVILEGES tkp;
+
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+	{
+		return FALSE;
+	}
+
+	if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &sedebugnameValue))
+	{
+		CloseHandle(hToken);
+		return FALSE;
+	}
+
+	tkp.PrivilegeCount = 1;
+	tkp.Privileges[0].Luid = sedebugnameValue;
+	tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	if (!AdjustTokenPrivileges(hToken, FALSE, &tkp, sizeof tkp, NULL, NULL))
+	{
+		CloseHandle(hToken);
+	}
+
 	return TRUE;
 }
 
@@ -155,15 +272,15 @@ BOOL CR3APIHookScanner::CbCollectProcessInfo(PPROCESS_INFO pProcessInfo, PBOOL p
 	return TRUE;
 }
 
-BOOL CR3APIHookScanner::CbCollectModuleInfo(PMODULE_INFO pModuleInfo)
+BOOL CR3APIHookScanner::CbCollectModuleInfo(PPROCESS_INFO pProcessInfo, PMODULE_INFO pModuleInfo)
 {
-	if (NULL == pModuleInfo)
+	if (NULL == pModuleInfo || NULL == pProcessInfo)
 	{
 		return FALSE;
 	}
 
 	printf("Module:%ls\n", pModuleInfo->szModuleName);
-	m_vecModuleInfo.push_back(pModuleInfo);
+	pProcessInfo->m_vecModuleInfo.push_back(pModuleInfo);
 
 	return TRUE;
 }
--- R3UnhookDemo/CR3APIHookScanner.h
+++ R3UnhookDemo/CR3APIHookScanner.h
@@ -2,21 +2,28 @@
 #include "stdafx.h"
 #define MAX_PROCESS_LEN	520
 #define MAX_MODULE_LEN	520
+#define MAX_MODULE_PATH	1024
 
 #include <vector>
 
 using namespace std;
 
+typedef struct MODULE_INFO_
+{
+	BYTE* pDllBaseAddr;
+	DWORD dwSizeOfImage;
+	WCHAR szModuleName[MAX_MODULE_LEN];
+	WCHAR szModulePath[MAX_MODULE_PATH];
+}MODULE_INFO, * PMODULE_INFO;
+
 typedef struct PROCESS_INFO_
 {
 	DWORD dwProcessId;
 	WCHAR szProcessName[MAX_PROCESS_LEN];
+	//todo：智能指针？
+	vector<MODULE_INFO*> m_vecModuleInfo;
 }PROCESS_INFO, * PPROCESS_INFO;
 
-typedef struct MODULE_INFO_
-{
-	WCHAR szModuleName[MAX_MODULE_LEN];
-}MODULE_INFO, *PMODULE_INFO;
 
 //class PROCESS_INFO
 //{
@@ -32,6 +39,7 @@ typedef BOOL (WINAPI* CALLBACK_EMUNPROCESS)(
 	PBOOL pBreak);
 
 typedef BOOL (WINAPI* CALLBACK_EMUNMODULE)(
+	PPROCESS_INFO pProcessInfo,
 	PMODULE_INFO pModuleInfo);
 
 class CR3APIHookScanner
@@ -39,21 +47,53 @@ class CR3APIHookScanner
 public:
 	CR3APIHookScanner();
 	~CR3APIHookScanner();
+	
+	//todo:实现单例、禁止拷贝或拷贝上的深拷贝
 
+	/**
+	* 扫描全部进程是否挂钩
+	*
+	* @return
+	*/
 	BOOL ScanAllProcesses();
+
+	/**
+	* 扫描指定Id的进程是否挂钩
+	*
+	* @param szPath : 待扫描进程的进程Id
+	* @return
+	*/
 	BOOL ScanSingleProcessById(DWORD dwProcessId);
 	BOOL ScanSingleProcessByName(CONST PCHAR pProcessName);
 
 private:
 	BOOL Init();
 	BOOL Release();
+	BOOL Clear();
 	BOOL EmurateProcesses(CALLBACK_EMUNPROCESS pCallbackFunc);
-	BOOL EmurateModules(DWORD dwProcessId, CALLBACK_EMUNMODULE pCallbackFunc);
+	BOOL EmurateModules(PPROCESS_INFO pProcessInfo, CALLBACK_EMUNMODULE pCallbackFunc);
+	BOOL ScanSingle(PPROCESS_INFO pProcessInfo);
+	BOOL LoadDllImage(PWCHAR pDllPath);
+
+	/**
+	* 根据Dll实际加载到的地址，修复Dll映像的地址
+	*
+	* @return
+	*/
+	BOOL FixRelocData();
+
+	BOOL EnableDebugPrivelege();
+	//回调函数
 	static BOOL CbCollectProcessInfo(PPROCESS_INFO pProcessInfo, PBOOL pBreak);
-	static BOOL CbCollectModuleInfo(PMODULE_INFO pModuleInfo);
+	static BOOL CbCollectModuleInfo(PPROCESS_INFO pProcessInfo, PMODULE_INFO pModuleInfo);
 
 private:
 	static vector<PROCESS_INFO*> m_vecProcessInfo;
-	static vector<MODULE_INFO*> m_vecModuleInfo;
+	//static vector<MODULE_INFO*> m_vecModuleInfo;
 	static int m_test;
 };
+
+
+//参考：
+//https://github.com/czp541308303/AntiHook/blob/main/scanhook.cpp
+//https://github.com/NtRaiseHardError/Antimalware-Research/blob/master/Generic/Userland%20Hooking/AntiHook/AntiHook/AntiHook/AntiHook.c
